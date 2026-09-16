66fc3e5194b324a86c40067cd85dc205f1e08763
Omar Ahmed <omarco0999@gmail.com>
2026-09-16 22:26:13 +0300

    resolve

--- src/Core/Plugin.php
+++ src/Core/Plugin.php
@@ -98,12 +98,6 @@ final class Plugin
         $services->branchCatalogFilter()->register();
 
         /*
-         * Filter WooCommerce Local Pickup
-         * according to the currently active branch.
-         */
-        $services->branchPickupFilter()->register();
-
-        /*
          * Save branch information into the order.
          */
         $services->orderMetaManager()->register();
